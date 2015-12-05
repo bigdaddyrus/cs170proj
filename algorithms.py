a9b9e3e9a1bba689aab867204699c85f53a6090d
bigdaddyrus <1994ep@gmail.com>
2015-12-04 17:51:30 -0800

    reformat

--- algorithms.py
+++ algorithms.py
@@ -1,6 +1,8 @@
 import sys
 import random
+from util import *
 
+# Run a single instance
 # By default algorithm will name the ouput file as INPUTFILE.out
 def main(argv):
 	if len(argv) < 2:
@@ -23,37 +25,6 @@ def simple_random(size):
 		lst_of_nodes.append(i + 1)
 	random.shuffle(lst_of_nodes)
 	return lst_of_nodes
-
-
-# Helper Functions ---------------------------
-
-def get_size(file_buffer):
-	first_line = file_buffer.readline()
-	return int(first_line)
-
-
-def write_result(result_list, outputfile):
-	fout = open(outputfile, "w")
-	length = len(result_list)
-	for i in xrange(length):
-		fout.write(str(result_list[i]))
-		if i == length - 1:
-			fout.write("\n")
-		fout.write(" ")
-	fout.close()
-
-
-def write_all_results(result_lists, outputfile):
-	fout = open(outputfile, "w")
-	for result_list in result_lists:
-		length = len(result_list)
-		for i in xrange(length):
-			fout.write(str(result_list[i]))
-			if i == length - 1:
-				fout.write("\n")
-			else:
-				fout.write(" ")
-	fout.close()
 
 
 if __name__ == '__main__':
